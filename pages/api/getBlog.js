5d69e16b0cbd8ceae77c5e4795d495772c209cac
Ashwini <ashwiniparaye9309@gmail.com>
2023-04-03 20:33:23 +0530

    inner html

--- pages/api/getBlog.js
+++ pages/api/getBlog.js
@@ -1,17 +1,15 @@
-// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// // http://localhost:3000/api/getBlog?slug=how-to-learn-next
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// http://localhost:3000/api/getBlog?slug=how-to-learn-next
 
-// const fs = require('fs');
+const fs = require("fs");
 
-// export default function handler(req, res) {
-//   fs.readFile(`blogdata/${req.query.slug}.json`, 'utf-8', (err, data) =>{
-//     if (err){
-//       res.status(500).json( { err: "No such blog not found" } )
-//     }
-//     console.log(req.query.slug)
+export default function handler(req, res) {
+  fs.readFile(`blogdata/${req.query.slug}.json`, "utf-8", (err, data) => {
+    if (err) {
+      res.status(500).json({ err: "No such blog not found" });
+    }
+    console.log(req.query.slug);
 
-//     res.status(200).json( JSON.parse(data) )
-
-//   })
-
-// }
+    res.status(200).json(JSON.parse(data));
+  });
+}
