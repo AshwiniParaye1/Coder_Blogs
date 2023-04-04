14f54c246b5026af0beb836de24a21f246a8036d
Ashwini <ashwiniparaye9309@gmail.com>
2023-04-04 17:28:24 +0530

    contact api

--- pages/api/postContact.js
+++ pages/api/postContact.js
@@ -3,16 +3,14 @@ import * as fs from "fs";
 export default async function handler(req, res) {
   if (req.method === "POST") {
     // Process a POST request
-    let data = await fs.promises.readdir("contactData");
-    console.log(data);
+    let data = await fs.promises.readdir("contactdata");
     fs.promises.writeFile(
-      `contactData/${data.length + 1}.json`,
+      `contactdata/${data.length + 1}.json`,
       JSON.stringify(req.body)
     );
-    res.status(200).json(req);
+    res.status(200).json({ success: true });
   } else {
     // Handle any other HTTP method
     res.status(200).json(["allBlogs"]);
-    //   name, email, desc, phone
   }
 }
